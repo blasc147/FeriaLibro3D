1ffb4816295331e3b9bc6d12bd7d28db9a16b3c7
blasc147 <blasc147@gmail.com>
2020-07-21 14:32:19 -0300

    imagen en la pantalla

--- js/sala.js
+++ js/sala.js
@@ -6,136 +6,18 @@ import { RectAreaLightHelper } from '../jsm/helpers/RectAreaLightHelper.js';
 import { RectAreaLightUniformsLib } from '../jsm/lights/RectAreaLightUniformsLib.js';
 //import { CSS3DRenderer, CSS3DObject } from '/jsm/renderers/CSS3DRenderer.js';
 import {OBJLoader} from "../jsm/loaders/OBJLoader.js";
+import {MTLLoader} from "../jsm/loaders/MTLLoader.js";
+import {DDSLoader} from "../jsm/loaders/DDSLoader.js";
 
 
-// shader injection for box projected cube environment mapping
-var worldposReplace = /* glsl */`
-#define BOX_PROJECTED_ENV_MAP
-#if defined( USE_ENVMAP ) || defined( DISTANCE ) || defined ( USE_SHADOWMAP )
-  vec4 worldPosition = modelMatrix * vec4( transformed, 1.0 );
-  #ifdef BOX_PROJECTED_ENV_MAP
-    vWorldPosition = worldPosition.xyz;
-  #endif
-#endif
-`;
-var Element = function ( id, x, y, z, ry ) {
 
-				var div = document.createElement( 'div' );
-				div.style.width = '480px';
-				div.style.height = '360px';
-				div.style.backgroundColor = '#000';
-
-				var iframe = document.createElement( 'iframe' );
-				iframe.style.width = '480px';
-				iframe.style.height = '360px';
-				iframe.style.border = '0px';
-				iframe.src = [ 'https://www.youtube.com/embed/', id, '?rel=0' ].join( '' );
-				div.appendChild( iframe );
-
-				var object = new CSS3DObject( div );
-				object.position.set( x, y, z );
-				object.rotation.y = ry;
-
-				return object;
-
-			};
-var envmapPhysicalParsReplace = /* glsl */`
-#if defined( USE_ENVMAP )
-  #define BOX_PROJECTED_ENV_MAP
-  #ifdef BOX_PROJECTED_ENV_MAP
-    uniform vec3 cubeMapSize;
-    uniform vec3 cubeMapPos;
-    varying vec3 vWorldPosition;
-    vec3 parallaxCorrectNormal( vec3 v, vec3 cubeSize, vec3 cubePos ) {
-      vec3 nDir = normalize( v );
-      vec3 rbmax = ( .5 * cubeSize + cubePos - vWorldPosition ) / nDir;
-      vec3 rbmin = ( -.5 * cubeSize + cubePos - vWorldPosition ) / nDir;
-      vec3 rbminmax;
-      rbminmax.x = ( nDir.x > 0. ) ? rbmax.x : rbmin.x;
-      rbminmax.y = ( nDir.y > 0. ) ? rbmax.y : rbmin.y;
-      rbminmax.z = ( nDir.z > 0. ) ? rbmax.z : rbmin.z;
-      float correction = min( min( rbminmax.x, rbminmax.y ), rbminmax.z );
-      vec3 boxIntersection = vWorldPosition + nDir * correction;
-      return boxIntersection - cubePos;
-    }
-  #endif
-  #ifdef ENVMAP_MODE_REFRACTION
-    uniform float refractionRatio;
-  #endif
-  vec3 getLightProbeIndirectIrradiance( /*const in SpecularLightProbe specularLightProbe,*/ const in GeometricContext geometry, const in int maxMIPLevel ) {
-    vec3 worldNormal = inverseTransformDirection( geometry.normal, viewMatrix );
-    #ifdef ENVMAP_TYPE_CUBE
-      #ifdef BOX_PROJECTED_ENV_MAP
-        worldNormal = parallaxCorrectNormal( worldNormal, cubeMapSize, cubeMapPos );
-      #endif
-      vec3 queryVec = vec3( flipEnvMap * worldNormal.x, worldNormal.yz );
-      // TODO: replace with properly filtered cubemaps and access the irradiance LOD level, be it the last LOD level
-      // of a specular cubemap, or just the default level of a specially created irradiance cubemap.
-      #ifdef TEXTURE_LOD_EXT
-        vec4 envMapColor = textureCubeLodEXT( envMap, queryVec, float( maxMIPLevel ) );
-      #else
-        // force the bias high to get the last LOD level as it is the most blurred.
-        vec4 envMapColor = textureCube( envMap, queryVec, float( maxMIPLevel ) );
-      #endif
-      envMapColor.rgb = envMapTexelToLinear( envMapColor ).rgb;
-    #elif defined( ENVMAP_TYPE_CUBE_UV )
-      vec4 envMapColor = textureCubeUV( envMap, worldNormal, 1.0 );
-    #else
-      vec4 envMapColor = vec4( 0.0 );
-    #endif
-    return PI * envMapColor.rgb * envMapIntensity;
-  }
-  // Trowbridge-Reitz distribution to Mip level, following the logic of http://casual-effects.blogspot.ca/2011/08/plausible-environment-lighting-in-two.html
-  float getSpecularMIPLevel( const in float roughness, const in int maxMIPLevel ) {
-    float maxMIPLevelScalar = float( maxMIPLevel );
-    float sigma = PI * roughness * roughness / ( 1.0 + roughness );
-    float desiredMIPLevel = maxMIPLevelScalar + log2( sigma );
-    // clamp to allowable LOD ranges.
-    return clamp( desiredMIPLevel, 0.0, maxMIPLevelScalar );
-  }
-  vec3 getLightProbeIndirectRadiance( /*const in SpecularLightProbe specularLightProbe,*/ const in vec3 viewDir, const in vec3 normal, const in float roughness, const in int maxMIPLevel ) {
-    #ifdef ENVMAP_MODE_REFLECTION
-      vec3 reflectVec = reflect( -viewDir, normal );
-      // Mixing the reflection with the normal is more accurate and keeps rough objects from gathering light from behind their tangent plane.
-      reflectVec = normalize( mix( reflectVec, normal, roughness * roughness) );
-    #else
-      vec3 reflectVec = refract( -viewDir, normal, refractionRatio );
-    #endif
-    reflectVec = inverseTransformDirection( reflectVec, viewMatrix );
-    float specularMIPLevel = getSpecularMIPLevel( roughness, maxMIPLevel );
-    #ifdef ENVMAP_TYPE_CUBE
-      #ifdef BOX_PROJECTED_ENV_MAP
-        reflectVec = parallaxCorrectNormal( reflectVec, cubeMapSize, cubeMapPos );
-      #endif
-      vec3 queryReflectVec = vec3( flipEnvMap * reflectVec.x, reflectVec.yz );
-      #ifdef TEXTURE_LOD_EXT
-        vec4 envMapColor = textureCubeLodEXT( envMap, queryReflectVec, specularMIPLevel );
-      #else
-        vec4 envMapColor = textureCube( envMap, queryReflectVec, specularMIPLevel );
-      #endif
-      envMapColor.rgb = envMapTexelToLinear( envMapColor ).rgb;
-    #elif defined( ENVMAP_TYPE_CUBE_UV )
-      vec4 envMapColor = textureCubeUV( envMap, reflectVec, roughness );
-    #elif defined( ENVMAP_TYPE_EQUIREC )
-      vec2 sampleUV = equirectUv( reflectVec );
-      #ifdef TEXTURE_LOD_EXT
-        vec4 envMapColor = texture2DLodEXT( envMap, sampleUV, specularMIPLevel );
-      #else
-        vec4 envMapColor = texture2D( envMap, sampleUV, specularMIPLevel );
-      #endif
-      envMapColor.rgb = envMapTexelToLinear( envMapColor ).rgb;
-    #endif
-    return envMapColor.rgb * envMapIntensity;
-  }
-#endif
-`;
 
 // scene size
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 
 // camera
-var VIEW_ANGLE = 45;
+var VIEW_ANGLE = 70;
 var ASPECT = WIDTH / HEIGHT;
 var NEAR = 1;
 var FAR = 800;
@@ -144,7 +26,7 @@ var camera, cubeCamera, scene, renderer;
 
 var cameraControls;
 
-var groundPlane, wallMat;
+var groundPlane, wallMat, secondPlane, light1;
 
 init();
 
@@ -154,7 +36,7 @@ function init() {
 
 
   //actualizar escena
-  update();
+  //update();
 
 }
 
@@ -167,35 +49,13 @@ function createScene(){
   renderer.setSize( WIDTH, HEIGHT );
   container.appendChild( renderer.domElement );
 
-  // gui controls
-  var gui = new GUI();
-  var params = {
-    'box projected': true
-  };
-  var bpcemGui = gui.add( params, 'box projected' );
-
-  bpcemGui.onChange( function ( value ) {
-
-    if ( value ) {
-
-      groundPlane.material = boxProjectedMat;
-
-    } else {
-
-      groundPlane.material = defaultMat;
-
-    }
-
-    render();
-
-  } );
 
   // scene
   scene = new THREE.Scene();
 
   // camera
   camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR );
-  camera.position.set( 312, 56, - 130 );
+  camera.position.set( 180, 80, 0 );
 
 
   cameraControls = new OrbitControls( camera, renderer.domElement );
@@ -219,209 +79,257 @@ function createScene(){
 
   // piso
   var loader = new THREE.TextureLoader();
-  var rMap = loader.load( '/assets/models/textures/hardwood2_diffuse.jpg' );
+  var rMap = loader.load( '/assets/models/textures/FloorsCheckerboard_S_Diffuse.jpg' );
   rMap.wrapS = THREE.RepeatWrapping;
   rMap.wrapT = THREE.RepeatWrapping;
-  rMap.repeat.set( 10, 10 );
+  rMap.repeat.set(8, 8);
 
-  var defaultMat = new THREE.MeshPhysicalMaterial( {
-    roughness: 1,
-    envMap: cubeRenderTarget.texture,
-    roughnessMap: rMap
+  var defaultMat = new THREE.MeshBasicMaterial( {
+     map: rMap,
+     color:0x484849
   } );
 
 
 
-  groundPlane = new THREE.Mesh( new THREE.PlaneBufferGeometry( 500, 300, 300 ), defaultMat );
+  groundPlane = new THREE.Mesh( new THREE.PlaneBufferGeometry( 350, 300, 300 ), defaultMat );
   groundPlane.rotateX( - Math.PI / 2 );
-  groundPlane.position.set( 0, - 100, 0 );
+  groundPlane.position.set( 25, - 100, 0 );
   scene.add( groundPlane );
+
+
+
+  //columnas
+
+  var entrePiso = new THREE.BoxBufferGeometry( 100, 10, 300);
+  entrePiso.translate( 150, -50, 0 );
+  entrePiso.rotateZ( Math.PI / 12);
+  var material = new THREE.MeshLambertMaterial( { color: 0x484848, flatShading: true } );
+  var ep = new THREE.Mesh( entrePiso, material );
+  scene.add( ep );
+
+  crearColumna(250,10,10,-10,-22,-150);
+  crearColumna(250,10,10,-10,-22,150);
+  crearColumna(10,250,10,-10,0,-150);
+  crearColumna(10,250,10,-10,0,150);
+  crearColumna(10,30,300,110,-10,0);
+  crearColumna(10,10,300,200,100,0);
+  crearColumna(50,40,300,-125,-100,0);
+
+
+  function crearColumna(x, y, z, a ,b ,c ){
+    let col1 = new THREE.BoxBufferGeometry( x,y,z);
+    col1.translate( a,b,c );
+    let cola = new THREE.Mesh( col1, material );
+    scene.add( cola );
+  }
 
   // paredes de ladrillo
   var diffuseTex = loader.load( '/assets/models/textures/brick_diffuse.jpg', function () {
 
-    updateCubeMap();
+    //updateCubeMap();
 
   } );
   diffuseTex.wrapS = THREE.RepeatWrapping;
   diffuseTex.wrapT = THREE.RepeatWrapping;
-  diffuseTex.repeat.set( 5,5 );
+  diffuseTex.repeat.set( 3,3 );
 
-  var bumpTex = loader.load( '/assets/models/textures/brick_bump.jpg', function () {
 
-    updateCubeMap();
-
-  } );
-  bumpTex.wrapS = THREE.RepeatWrapping;
-  bumpTex.wrapT = THREE.RepeatWrapping;
-  bumpTex.repeat.set( 100, 100 );
 
   wallMat = new THREE.MeshPhysicalMaterial( {
     map: diffuseTex,
-    bumpMap: bumpTex,
+
     bumpScale: 0.1,
   } );
 
 //posiciones de las paredes y tamano
-  var planeGeo = new THREE.PlaneBufferGeometry( 300, 250 );
+  var paredes = new THREE.PlaneBufferGeometry( 350, 250 );
 
-  var planeBack1 = new THREE.Mesh( planeGeo, wallMat );
+  var planeBack1 = new THREE.Mesh( paredes, wallMat );
   planeBack1.position.z = - 150;
-  planeBack1.position.x = - 50;
+  planeBack1.position.x = 25;
   scene.add( planeBack1 );
 
-  var planeBack2 = new THREE.Mesh( planeGeo, wallMat );
-  planeBack2.position.z = - 150;
-  planeBack2.position.x = 50;
-  scene.add( planeBack2 );
 
-  var planeFront1 = new THREE.Mesh( planeGeo, wallMat );
+
+  var planeFront1 = new THREE.Mesh( paredes, wallMat );
   planeFront1.position.z = 150;
-  planeFront1.position.x = - 50;
+  planeFront1.position.x = 25;
   planeFront1.rotateY( Math.PI );
   scene.add( planeFront1 );
 
-  var planeFront2 = new THREE.Mesh( planeGeo, wallMat );
-  planeFront2.position.z = 150;
-  planeFront2.position.x = 50;
-  planeFront2.rotateY( Math.PI );
-  scene.add( planeFront2 );
+  var paredes2 = new THREE.PlaneBufferGeometry(300, 250 );
 
-  var planeRight = new THREE.Mesh( planeGeo, wallMat );
+  var planeRight = new THREE.Mesh( paredes2, wallMat );
   planeRight.position.x =200;
   planeRight.rotateY( - Math.PI / 2 );
   scene.add( planeRight );
 
-  var planeLeft = new THREE.Mesh( planeGeo, wallMat );
-  planeLeft.position.x = - 200;
+  var planeLeft = new THREE.Mesh( paredes2, wallMat );
+  planeLeft.position.x = -150;
   planeLeft.rotateY( Math.PI / 2 );
   scene.add( planeLeft );
 
   //lights
   var width = 200;
   var height = 100;
-  var intensity = 6;
+  var intensity = 10;
 
   RectAreaLightUniformsLib.init();
+//reflectores
 
-  //var blueRectLight = new THREE.RectAreaLight( 0xf3aaaa, intensity, width, height );
-  //blueRectLight.position.set( 200, 5, 0 );
-  //blueRectLight.lookAt( 10, 5, 0 );
-  //scene.add( blueRectLight );
 
-//  var blueRectLightHelper = new RectAreaLightHelper( blueRectLight, 0xffffff );
-  //blueRectLight.add( blueRectLightHelper );
+  crearReflectores(-80,-30,-148,0xf3aaaa);
+  crearReflectores(50,-30,-148,0xf3aaaa);
+  crearReflectores(-80,-30,148,0xf3aaaa);
+  crearReflectores(50,-30,148,0xf3aaaa);
+  crearReflectores(198,95,-40,0xf3aaaa);
+  crearReflectores(198,95,60,0xf3aaaa);
+  crearReflectores(198,-10,-40,0xf3aaaa);
+  crearReflectores(198,-10,60,0xf3aaaa);
 
-  var redRectLight = new THREE.RectAreaLight( 0x9aaeff, intensity, width, height );
-  redRectLight.position.set( -200, 5, 0 );
+
+
+
+
+//luz pantalla
+  var redRectLight = new THREE.RectAreaLight( 0xf3aaaa, intensity, width, height );
+  redRectLight.position.set( -150, 5, 0 );
   redRectLight.lookAt( 100, 5, 0 );
   scene.add( redRectLight );
 
   var redRectLightHelper = new RectAreaLightHelper( redRectLight, 0xffffff );
   redRectLight.add( redRectLightHelper );
 
-  //groupSillas
-  //chair.position.y = -90;
-  //chair.position.x = -70;
-  //chair.position.z = 0;
 
-  var posiX=-75;
-  var posiz=-45;
-  var posiy=-90;
+  var posiX=-60;
+  var posiza=-130;
+  var posiy=-100;
+  var poszb=90;
+//crear sillas laterales
+for(let j=0;j<3;j++){
 
-createSillas(-80, -130,-90, 1.4);
-createSillas(posiX, posiz,posiy,1.58);
-createSillas(posiX, 0,posiy,1.58);
-createSillas(posiX, 85,posiy,1.8);
+    createSillas(posiX, posiza,posiy, 1.4,6);
+    createSillas(130, posiza,-10, 1.4,0);
+    createSillas(160, posiza,-5, 1.4,0);
 
+    createSillas(posiX,poszb,posiy,1.8,6);
+    createSillas(130,poszb,-10,1.8,0);
+    createSillas(160,poszb,-5,1.8,0);
+    posiza+=15;
+    poszb+=15;
 
+}
+//sillas del medio
+var poszc=-58
+for(let j=0;j<8;j++){
 
+  createSillas(posiX, poszc,posiy,1.58,6);
+  createSillas(130, poszc,-10,1.58,0);
+  createSillas(160, poszc,-5,1.58,0);
 
+    poszc+=15;
 
-  render();
 }
 
-function createSillas(x,z,y, rota){
+
+var planeGeoImg = new THREE.PlaneGeometry(200, 100, 1, 1);
+var textureImg = new THREE.TextureLoader().load( '/assets/models/textures/fdld22-logo.png' );
+var planeImg = new THREE.MeshLambertMaterial( {
+  map: textureImg,
+  color:0xffffff,
+  transparent:true,
+  opacity: 0.2
+
+ } );
+var plane = new THREE.Mesh(planeGeoImg, planeImg);
+plane.rotateY( Math.PI/2 );
+
+//plane.receiveShadow = true;
+plane.position.set( -140, 5, 0 );
+scene.add(plane);
+  render();
+
+  // instantiate a loader
+}
+
+function createSillas(x,z,y, rota, cant){
 
   var chair;
   var groupSillas = new THREE.Group();
-    for(let i=0;i<=6;i++){
+  // model
+
+				var onProgress = function ( xhr ) {
+
+					if ( xhr.lengthComputable ) {
+
+						var percentComplete = xhr.loaded / xhr.total * 100;
+						console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+
+					}
+
+				};
+
+		var onError = function () { };
+
+    var manager = new THREE.LoadingManager();
+    manager.addHandler( /\.dds$/i, new DDSLoader() );
+    for(let i=0;i<=cant;i++){
       // instantiate a loader
-        var loader = new OBJLoader();
-
-        // load a resource
-        loader.load(
-        // resource URL
-        "/assets/models/theater-chair.obj",
-        // called when resource is loaded
-        function ( object ) {
-        chair = object;
-        if (chair){
-
-            chair.position.y = y;
-            chair.position.x = x;
-            chair.position.z = z;
-            chair.rotation.y-=rota;
-            chair.scale.set(0.2,0.2,0.2);
-            x+=30;
-
-            groupSillas.add(chair);
-        }
 
 
-        },
-        // called when loading is in progresses
-        function ( xhr ) {
 
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        var loader = new MTLLoader( manager )
+					.setPath( '/assets/models/' )
+					.load( 'untitled.mtl', function ( materials ) {
 
-        },
-        // called when loading has errors
-        function ( error ) {
+						materials.preload();
 
-            console.log( 'An error happened' );
+						new OBJLoader( manager )
+							.setMaterials( materials )
+							.setPath( '/assets/models/' )
+							.load( 'untitled.obj', function ( object ) {
+                chair = object;
 
-        }
-        );
+                if (chair){
+
+                    chair.position.y = y;
+                    chair.position.x = x;
+                    chair.position.z = z;
+                    chair.rotation.y-=rota;
+                    chair.scale.set(0.6,0.6,0.6);
+                    x+=30;
+                    groupSillas.add(chair);
+                    render();
+                }
+
+							}, onProgress, onError );
+
+					} );
+
 
 
           }
         scene.add(groupSillas);
 }
 
-
-function updateCubeMap() {
-
-  //disable specular highlights on walls in the environment map
-  wallMat.roughness = 1;
-
-  groundPlane.visible = false;
-
-  cubeCamera.position.copy( groundPlane.position );
-
-  cubeCamera.update( renderer, scene );
-
-  wallMat.roughness = 0.6;
-
-  groundPlane.visible = true;
-
-
-  render();
+function crearReflectores(x, y, z, color){
+  var intensidad = 4;
+	var distance = 150;
+	var decay = 3.0;
+  light1 = new THREE.PointLight( color, intensidad, distance, decay );
+  light1.position.set(x,y,z);
+  var sphere = new THREE.CylinderGeometry( 2, 5, 5, 8 );
+	light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: color } ) ) );
+	scene.add( light1 );
 
 }
 
-function update(){
-    //la libreria tiene una funcion requestAnimationFrame(update) para generar el loop
-    requestAnimationFrame(update);
-    render();
-
-}
 
 
 function render() {
 
   renderer.render( scene, camera );
-  camera.position.x -= Math.cos( Math.sin( 10 ) ) / 10;
-  camera.position.z += Math.cos( Math.sin( 10 ) ) / 10;
-
+  //requestAnimationFrame(render);
+  camera.position.x -= Math.cos( Math.sin( 10 ) ) / 50;
+  camera.position.y -= Math.cos( Math.sin( 10 ) ) / 50;
 }
--- js/video.js
+++ js/video.js
@@ -1,85 +1,106 @@
-import * as THREE from "/build/three.module.js";
-import {OrbitControls} from "/jsm/controls/OrbitControls.js";
-import {CSS3DRenderer,CSS3DObject} from "/jsm/renderers/CSS3DRenderer.js";
-import {TrackballControls} from "/jsm/controls/TrackballControls.js";
+"use strict";
+import * as THREE from '../build/three.module.js';
+import { CSS3DRenderer, CSS3DObject } from 'https://unpkg.com/three/examples/jsm/renderers/CSS3DRenderer.js';
+import { TrackballControls } from 'https://unpkg.com/three/examples/jsm/controls/TrackballControls.js';
 
-var camera, scene, renderer;
-			var controls;
-			var Element = function ( id, x, y, z, ry ) {
-				var div = document.createElement( 'div' );
-				div.style.width = '480px';
-				div.style.height = '360px';
-				div.style.backgroundColor = '#000';
-				var iframe = document.createElement( 'iframe' );
-				iframe.style.width = '480px';
-				iframe.style.height = '360px';
-				iframe.style.border = '0px';
-				iframe.src = [ 'https://www.youtube.com/embed/', id, '?rel=0&autoplay=1&mute=1' ].join( '' );
-				div.appendChild( iframe );
-				var object = new CSS3DObject( div );
-				object.position.set( x, y, z );
-				object.rotation.y = ry;
-				return object;
-      };
+var camera, webglscene, webglrenderer;
+var cssscene, cssrenderer;
+var controls;
+var geometry, material, mesh;
 
-			init();
-			animate();
+init();
+animate();
 
-			function init() {
+function createYoutubeVideo ( id, x, y, z, ry ) {
 
-				var container = document.getElementById( 'container' );
+  var div = document.createElement( 'div' );
+  div.style.width = '480px';
+  div.style.height = '360px';
+  div.style.backgroundColor = '#fff';
 
-				camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 5000 );
-				camera.position.set( 50, 50, 700 );
+  var iframe = document.createElement( 'iframe' );
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = '0px';
+  iframe.src = [ 'https://www.youtube.com/embed/', id, '?rel=0' ].join( '' );
+  div.appendChild( iframe );
 
-				scene = new THREE.Scene();
+  var cssobject = new CSS3DObject( div );
+  cssobject.position.set( x, y, z );
+  cssobject.rotation.y = ry;
+  cssobject.scale.set(0.01, 0.01, 0.01);
 
-				renderer = new CSS3DRenderer();
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
+  cssscene.add(cssobject);
 
-				var group = new THREE.Group();
-				group.add( new Element( 'TlLijkYQjlw', 0, 0, 240, 0 ) );
-				//group.add( new Element( 'RbtgTFGDkxA', 240, 0, 0, Math.PI / 2 ) );
-				//group.add( new Element( 'fuyZFMRtXGs', 0, 0, - 240, Math.PI ) );
-				//group.add( new Element( 'uqj9j-qz4AE', - 240, 0, 0, - Math.PI / 2 ) );
-				scene.add( group );
+  var material = new THREE.MeshPhongMaterial({
+    opacity	: 0.0,
+    color	: new THREE.Color('black'),
+    blending: THREE.NoBlending,
+    side	: THREE.DoubleSide,
+  });
 
-				//controls = new TrackballControls( camera, renderer.domElement );
-				//controls.rotateSpeed = 4;
+  var geometry = new THREE.PlaneGeometry( 480, 360 );
+  var webglrepresentation = new THREE.Mesh( geometry, material );
+  webglrepresentation.position.copy( cssobject.position );
+  webglrepresentation.rotation.copy( cssobject.rotation );
+  webglrepresentation.scale.copy( cssobject.scale );
+  webglscene.add( webglrepresentation );
+}
 
-				window.addEventListener( 'resize', onWindowResize, false );
+function init() {
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
+  camera.position.z = 10;
 
-				// Block iframe events when dragging camera
+  webglscene = new THREE.Scene();
 
-				var blocker = document.getElementById( 'blocker' );
-				blocker.style.display = 'none';
+  webglrenderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  webglrenderer.setClearColor( 0x000000, 0 );
+  webglrenderer.setSize( window.innerWidth, window.innerHeight );
+  document.querySelector('#webgl').appendChild( webglrenderer.domElement );
 
-				document.addEventListener( 'mousedown', function () {
+  cssscene = new THREE.Scene();
+  cssrenderer = new CSS3DRenderer();
+  cssrenderer.setSize( window.innerWidth, window.innerHeight );
+  document.querySelector('#css').appendChild( cssrenderer.domElement );
 
-					blocker.style.display = '';
 
-				} );
-				document.addEventListener( 'mouseup', function () {
 
-					blocker.style.display = 'none';
+  geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  material = new THREE.MeshNormalMaterial();
 
-				} );
+  mesh = new THREE.Mesh( geometry, material );
+  webglscene.add( mesh );
 
-			}
+  createYoutubeVideo( 'FHVD9ft_ANw', 0, 0, -2, 0 );
 
-			function onWindowResize() {
+  var backPlaneGeometry = new THREE.PlaneGeometry(20, 10)
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-				renderer.setSize( window.innerWidth, window.innerHeight );
+  var backPlane = new THREE.Mesh(backPlaneGeometry, material);
+  backPlane.position.set(0,0,-5);
+  webglscene.add(backPlane);
 
-			}
+  controls = new TrackballControls( camera, webglrenderer.domElement );
+  controls.rotateSpeed = 4;
 
-			function animate() {
+  window.addEventListener( 'resize', onWindowResize, false );
+}
 
-				requestAnimationFrame( animate );
-				//controls.update();
-				renderer.render( scene, camera );
+function animate() {
 
-			}
+  requestAnimationFrame( animate );
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+  controls.update();
+  webglrenderer.render( webglscene, camera );
+  cssrenderer.render( cssscene, camera );
+
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  webglrenderer.setSize( window.innerWidth, window.innerHeight );
+  cssrenderer.setSize( window.innerWidth, window.innerHeight );
+}
